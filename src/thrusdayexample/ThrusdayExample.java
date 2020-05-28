7ce4c0dfc9df22dc03e9061fdad6f8c6d7594b56
shah161 <shah.161@sheridanc.on.ca>
2020-05-28 10:23:20 -0400

    first commit

--- src/thrusdayexample/ThrusdayExample.java
+++ src/thrusdayexample/ThrusdayExample.java
@@ -16,7 +16,7 @@ public class ThrusdayExample {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("testing");
     }
 
 }
